37dad86588e74be891d15f1d273742cd16c836da
Alan Maia <maiaalan@hotmail.com>
2019-01-30 22:41:54 -0300

    Header file done

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QLCDNumber>
 
 namespace Ui {
 class MainWindow;
@@ -16,7 +18,23 @@ public:
     ~MainWindow();
 
 private:
+    //Variables
     Ui::MainWindow *ui;
+    QPushButton *myButtons[16];
+    QLCDNumber *myLCD;
+    bool isDisplayReseted;
+    //Methods
+    void setDisplay();
+    void resetDisplay();
+    void sum();
+    void sub();
+    void mul();
+    void div();
+
+
+private  slots:
+    void buttonClicked();
 };
+
 
 #endif // MAINWINDOW_H
